3d6ef59ffb49047ed0e4571b1d2638d361cd20e4
TakodaS <austen.bolitho@protonmail.com>
2024-08-16 20:47:46 +0100

    add dream2nix support for building nodejs package derivations

--- flake.nix
+++ flake.nix
@@ -4,58 +4,81 @@
     devenv.url = "github:cachix/devenv";
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
+    dream2nix.url = "github:nix-community/dream2nix";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
     flake-parts,
-    nixpkgs,
-    process-compose-flake,
-    services-flake,
-    ...
+      nixpkgs,
+      process-compose-flake,
+      services-flake,
+      dream2nix,
+      gitignore,
+      ...
   }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.devenv.flakeModule
+  flake-parts.lib.mkFlake {inherit inputs;} {
+    imports = [
+      inputs.devenv.flakeModule
         process-compose-flake.flakeModule
-      ];
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
+    ];
+    systems = nixpkgs.lib.systems.flakeExposed;
+    perSystem = {
+      config,
+      self',
+      inputs',
+      pkgs,
+      system,
+      ...
+    }: {
+# Per-system attributes can be defined here. The self' and inputs'
+# module parameters provide easy access to attributes of the same
+# system.
+      packages.nodeapp = dream2nix.lib.evalModules {
+       packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          # Import our actual package definiton as a dream2nix module from ./default.nix
+          ./default.nix
+          {
+            # Aid dream2nix to find the project root. This setup should also works for mono
+            # repos. If you only have a single project, the defaults should be good enough.
+            paths.projectRoot = ./.;
+            # can be changed to ".git" or "flake.nix" to get rid of .project-root
+            paths.projectRootFile = "flake.nix";
+            paths.package = ./.;
+          }
+        ];
+      };
+      
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        formatter = pkgs.alejandra;
-        process-compose."default" = {config, ...}: {
-          imports = [
-            inputs.services-flake.processComposeModules.default
-          ];
+# Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+      formatter = pkgs.alejandra;
+      process-compose."default" = {config, ...}: {
+        imports = [
+          inputs.services-flake.processComposeModules.default
+        ];
 
-          # Add a pgweb process, that knows how to connect to our northwind db
-          settings.processes.storybook = let
-            pnpm = "${pkgs.corepack}/bin/pnpm";
-          in {command = "${pkgs.bash}/bin/bash -c '${pnpm} install && ${pnpm} storybook'";};
-        };
-        devenv.shells.default = {
-          # https://devenv.sh/reference/options/
-          packages = [pkgs.hello];
-          languages.javascript = {
+# Add a pgweb process, that knows how to connect to our northwind db
+        settings.processes.storybook = let
+          pnpm = "${pkgs.corepack}/bin/pnpm";
+        in {command = "${pkgs.bash}/bin/bash -c 'cd $out && ${pnpm} install && ${pnpm} storybook'";};
+      };
+      devenv.shells.default = {
+# https://devenv.sh/reference/options/
+        packages = [pkgs.hello];
+        languages.javascript = {
+          enable = true;
+          corepack = {
             enable = true;
-            corepack = {
-              enable = true;
-            };
           };
-          enterShell = ''
-            hello
-          '';
         };
+        enterShell = ''
+          hello
+          '';
       };
     };
+  };
 }
